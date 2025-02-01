49a6a41150a73acc4f26f3bc7cf093305146cf6a
ssol-park <ksttlov.naver@hanmail.net>
2025-02-01 18:24:44 +0900

    refactor: 미사용 코드 제거

--- src/main/java/com/psr/nosql/repository/ChannelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.psr.nosql.repository;
-
-
-import com.psr.nosql.entity.Channel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChannelRepository extends MongoRepository<Channel, String> {
-}
